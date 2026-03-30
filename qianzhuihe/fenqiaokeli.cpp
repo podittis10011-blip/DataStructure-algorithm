1ecd863815ce243eea2033f478590dc3ca3fdc0a
Sunshie <podittis10011@gmail.com>
2026-03-30 13:19:45 +0800

    统计方形test01

--- qianzhuihe/fenqiaokeli.cpp
+++ qianzhuihe/fenqiaokeli.cpp
@@ -1,41 +1,41 @@
-#include<cstdio>
-#include<cstring>
-#include<iostream>
-#include<algorithm>
+// #include<cstdio>
+// #include<cstring>
+// #include<iostream>
+// #include<algorithm>
 
-using namespace std;
+// using namespace std;
 
-const int N = 100010;
+// const int N = 100010;
 
-int n, k;
-int h[N], w[N];
+// int n, k;
+// int h[N], w[N];
 
-bool check(int mid)
-{
-    int res = 0;
-    for (int i = 0; i < n; i ++ )
-    {
-        res += (h[i] / mid) * (w[i] / mid);
-        if (res >= k) return true;
-    }
+// bool check(int mid)
+// {
+//     int res = 0;
+//     for (int i = 0; i < n; i ++ )
+//     {
+//         res += (h[i] / mid) * (w[i] / mid);
+//         if (res >= k) return true;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-int main()
-{
-    scanf("%d%d", &n, &k);
-    for (int i = 0; i < n; i ++ ) scanf("%d%d", &h[i], &w[i]);
+// int main()
+// {
+//     scanf("%d%d", &n, &k);
+//     for (int i = 0; i < n; i ++ ) scanf("%d%d", &h[i], &w[i]);
 
-    int l = 1, r = 1e5;
-    while (l < r)
-    {
-        int mid = l + r + 1 >> 1;
-        if (check(mid)) l = mid;
-        else r = mid - 1;
-    }
+//     int l = 1, r = 1e5;
+//     while (l < r)
+//     {
+//         int mid = l + r + 1 >> 1;
+//         if (check(mid)) l = mid;
+//         else r = mid - 1;
+//     }
 
-    printf("%d\n", r);
+//     printf("%d\n", r);
 
-    return 0;
-}
+//     return 0;
+// }
--- qianzhuihe/jiguangzhadan.cpp
+++ qianzhuihe/jiguangzhadan.cpp
@@ -11,46 +11,46 @@
     
 // }
 
-#include <cstring>
-#include <iostream>
-#include <algorithm>
+// #include <cstring>
+// #include <iostream>
+// #include <algorithm>
 
-using namespace std;
+// using namespace std;
 
-const int N = 5010;
+// const int N = 5010;
 
-int n, m;
-int s[N][N];
+// int n, m;
+// int s[N][N];
 
-int main()
-{
-    int cnt, R;
-    cin >> cnt >> R;
-    R = min(5001, R);
+// int main()
+// {
+//     int cnt, R;
+//     cin >> cnt >> R;
+//     R = min(5001, R);
 
-    n = m = R;
-    while (cnt -- )
-    {
-        int x, y, w;
-        cin >> x >> y >> w;
-        x ++, y ++ ;
-        n = max(n, x), m = max(m, y);
-        s[x][y] += w;
-    }
+//     n = m = R;
+//     while (cnt -- )
+//     {
+//         int x, y, w;
+//         cin >> x >> y >> w;
+//         x ++, y ++ ;
+//         n = max(n, x), m = max(m, y);
+//         s[x][y] += w;
+//     }
 
-    // 预处理前缀和
-    for (int i = 1; i <= n; i ++ )
-        for (int j = 1; j <= m; j ++ )
-            s[i][j] += s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1];
+//     // 预处理前缀和
+//     for (int i = 1; i <= n; i ++ )
+//         for (int j = 1; j <= m; j ++ )
+//             s[i][j] += s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1];
 
-    int res = 0;
+//     int res = 0;
 
-    // 枚举所有边长是R的矩形，枚举(i, j)为右下角
-    for (int i = R; i <= n; i ++ )
-        for (int j = R; j <= m; j ++ )
-            res = max(res, s[i][j] - s[i - R][j] - s[i][j - R] + s[i - R][j - R]);
+//     // 枚举所有边长是R的矩形，枚举(i, j)为右下角
+//     for (int i = R; i <= n; i ++ )
+//         for (int j = R; j <= m; j ++ )
+//             res = max(res, s[i][j] - s[i - R][j] - s[i][j - R] + s[i - R][j - R]);
 
-    cout << res << endl;
+//     cout << res << endl;
 
-    return 0;
-}
+//     return 0;
+// }
