2ab5372fc30349b7accc6638e15f8947e3def24b
Sunshie <podittis10011@gmail.com>
2026-05-21 09:54:20 +0800

    5.20栈的数组实现

---
+++ Stack/ListStack/Try02/listNode.java
@@ -0,0 +1,17 @@
+package Stack.ListStack.Try02;
+
+public class listNode {
+    private listNode next;
+    private int value;
+
+    //无参构造器
+    public listNode(){
+
+    }
+
+    //有参构造器
+    public listNode(int value){
+        this.value = value;
+    }
+    
+}
