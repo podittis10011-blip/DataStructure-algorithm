c1bf37dca2f1f82c803518f9a02ceced87be82ca
Sunshie <podittis10011@gmail.com>
2026-08-10 00:16:04 +0800

    luogu_p1886:单调队列/滑动窗口Commit01

---
+++ doublePointer/luogu_p1886/Main.java
@@ -0,0 +1,21 @@
+package doublePointer.luogu_p1886;
+
+import java.util.Deque;
+import java.util.Scanner;
+
+
+public class Main {
+    static int n,k;
+    static int[] a;
+
+    Deque<>
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();k = sc.nextInt();
+        a = new int[n + 1];
+        for(int i = 1;i <= n;i++){
+            a[i] = sc.nextInt();
+        }
+    }    
+}
