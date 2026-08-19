b44124a2178f7697f8c435706bdb8cfaa0c915e9
Sunshie <podittis10011@gmail.com>
2026-08-19 08:52:51 +0800

    8.19luogu_p1496-fixBugcommit

--- doublePointer/luogu_1496/Main.java
+++ doublePointer/luogu_1496/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     static int n,st,ed,sum;
 
-    class Line implements Comparable<Line>{
+    static class Line implements Comparable<Line>{
         public int l,r;
 
         public Line(){
@@ -33,18 +33,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        a = new Line[n + 2];
+        a = new Line[n + 1];
 
         for(int i = 1; i <= n;i++){
-            a[i].l = sc.nextInt();
-            a[i].r = sc.nextInt();
+            // a[i].l = sc.nextInt();
+            // a[i].r = sc.nextInt();
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            
+            a[i] = new Line(l,r);
         }
 
-        for(int i = 1; i <= n - 1;i++){
-            Arrays.sort(a,1,n + 1);
-        }
+        // for(int i = 1; i <= n - 1;i++){
+        //     Arrays.sort(a,1,n + 1);
+        // }
+        Arrays.sort(a,1,n + 1);
 
-        sum += a[1].r - a[1].l;
+        st = a[1].l;ed = a[1].r;
+        sum += ed - st;
         for(int i = 2; i <= n;i++){
             if(a[i].l < ed){
                 if(a[i].r < ed){
