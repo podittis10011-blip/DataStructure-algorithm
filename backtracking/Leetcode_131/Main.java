84fd78f64fa74280160f23f0fa7891fa56a66578
Sunshie <podittis10011@gmail.com>
2026-08-20 02:33:28 +0800

    8.20leetcode_131：分割回文串 ACcommit

---
+++ backtracking/Leetcode_131/Main.java
@@ -0,0 +1,66 @@
+package backtracking.Leetcode_131;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    static String s;
+
+    //结果集
+    static ArrayList<List<String>> result;
+
+    //结果
+    static ArrayList<String> path;
+
+    static void backtracking(String s,int startIndex){
+        if(startIndex >= s.length()){
+            result.add(new ArrayList<String>(path));
+            return;
+        }
+        for(int i = startIndex;i < s.length();i++){
+            if(isPalindrome(s,startIndex,i)){
+                String str = s.substring(startIndex,i + 1);
+                path.add(str);
+            }else{
+                continue;
+            }
+            backtracking(s, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    static boolean isPalindrome(String s,int start,int end){
+        for(int i = start,j = end;i < j;i++,j--){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<List<String>> partition(String s) {
+        backtracking(s,0);
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        s = sc.next();
+
+        result = new ArrayList<List<String>>();
+
+        path = new ArrayList<String>();
+
+        for(List<String> ans : partition(s)){
+            for(String s : ans){
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
