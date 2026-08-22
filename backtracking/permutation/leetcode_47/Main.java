e56cd03e7dd458d46f4eaee7479962bca9b6060c
Sunshie <podittis10011@gmail.com>
2026-08-22 17:45:14 +0800

    8.22commit

--- backtracking/permutation/leetcode_47/Main.java
+++ backtracking/permutation/leetcode_47/Main.java
@@ -59,3 +59,39 @@ public class Main {
         
     }
 }
+
+// leetcode
+// class Solution {
+
+//     static ArrayList<List<Integer>> result;
+//     static ArrayList<Integer> path;
+
+//     public static void backtracking(int[] nums,boolean[] used){
+//         if(path.size() == nums.length){
+//             result.add(new ArrayList<Integer>(path));
+//             return;
+//         }
+//         for(int i = 0;i < nums.length;i++){
+//             if(i > 0 && nums[i] == nums[i - 1] && used[i - 1] == false){
+//                 continue;
+//             }
+//             if(used[i] == false){
+//                 used[i] = true;
+//                 path.add(nums[i]);
+//                 backtracking(nums, used);
+//                 path.remove(path.size() - 1);
+//                 used[i] = false;
+//             }
+//         }
+//     }
+
+//     public List<List<Integer>> permuteUnique(int[] nums) {
+
+//         result = new ArrayList<List<Integer>>();
+//         path = new ArrayList<Integer>();
+//         Arrays.sort(nums);
+//         boolean[] used = new boolean[nums.length];
+//         backtracking(nums,used);
+//         return result;
+//     }
+// }
